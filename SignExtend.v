fa78a26fcf2e60312c142f5533da2a1856f2daa4
navidAdelpour <navidadelpour@gmail.com>
2018-12-17 18:46:55 +0330

    sign extend bug fixed

--- SignExtend.v
+++ SignExtend.v
@@ -6,7 +6,7 @@ module SignExtend(clock, instruction, output);
 	wire opcode = instruction[31:30];
 
 	always @ (clock) begin
-		case(opcode) begin
+		case(opcode)
 			2'b11: output = {{55{instruction[20]}}, instruction[20:12]};
 			2'b10: output = {{45{instruction[23]}}, instruction[23:5]};
 			2'b00: output = {{38{instruction[25]}}, instruction[25:0]};
