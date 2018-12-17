053063787073c4c53a8f803af3a1d9697b38e45a
navidAdelpour <navidadelpour@gmail.com>
2018-12-17 18:22:42 +0330

    fixing sign extend functionality

--- SignExtend.v
+++ SignExtend.v
@@ -1,13 +1,16 @@
-module SignExtend(clock, input_data, output_data);
-	input clock;	
-	input [31:0] input_data;
-	output reg [63:0] output_data;
-	
-	initial begin 
-		output_data = 0;
+module SignExtend(clock, instruction, output);
+
+	input clock;
+	input [31:0] instruction;
+	output reg [63:0] output;
+	wire opcode = instruction[31:30];
+
+	always @ (clock) begin
+		case(opcode) begin
+			2'b11: output = {{55{instruction[20]}}, instruction[20:12]};
+			2'b10: output = {{45{instruction[23]}}, instruction[23:5]};
+			2'b00: output = {{38{instruction[25]}}, instruction[25:0]};
+		endcase
 	end
 
-	always @ (posedge clock) begin
-		output_data = {{32{input_data[31]}}, input_data};
-	end
 endmodule
