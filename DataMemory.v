a6c78df3dc72cd2a8770a38cd7e682c4fb5e4238
navidAdelpour <navidadelpour@gmail.com>
2018-12-22 20:53:40 +0330

    fixing memory saving data

--- DataMemory.v
+++ DataMemory.v
@@ -1,18 +1,32 @@
-module DataMemory #(parameter n = 64, parameter size = 256) (clock, write, read, input_address, input_data, output_data);
+module DataMemory #(parameter size = 256) (clock, write, read, input_address, input_data, output_data);
 	input clock, write, read;
-	input [n - 1 : 0] input_address, input_data;
-	output[n - 1 : 0] output_data;
+	input [63 : 0] input_address, input_data;
+	output[63 : 0] output_data;
 
-	reg [n - 1 : 0] memory [0 : size - 1];
+	reg [7 : 0] memory [0 : size - 1];
 
+	assign output_data = read ? {
+		memory[input_address + 7],
+		memory[input_address + 6],
+		memory[input_address + 5],
+		memory[input_address + 4],
+		memory[input_address + 3],
+		memory[input_address + 2],
+		memory[input_address + 1],
+		memory[input_address + 0],
+	}
 
 	always @ (posedge clock) begin
-	
-		if(read)
-			output_data = memory[input_address];
 
 		if(write)
-			memory[input_address] = input_data;
+			memory[input_address + 7] = input_data[63 : 56];
+			memory[input_address + 6] = input_data[55 : 48];
+			memory[input_address + 5] = input_data[47 : 40];
+			memory[input_address + 4] = input_data[39 : 32];
+			memory[input_address + 3] = input_data[31 : 24];
+			memory[input_address + 2] = input_data[23 : 16];
+			memory[input_address + 1] = input_data[15 : 8];
+			memory[input_address + 0] = input_data[7 : 0];
 		
 	end
 
--- InstructionMemory.v
+++ InstructionMemory.v
@@ -1,10 +1,15 @@
-module InstructionMemory #(parameter n = 64, parameter size = 256) (input_address, output_data);
+module InstructionMemory #(parameter size = 256) (input_address, output_data);
 
-	input [n - 1 : 0] input_address;
-	output [n - 1 : 0] output_data;
+	input [63 : 0] input_address;
+	output [31 : 0] output_data;
 
-	reg [n - 1 : 0] memory [0 : size - 1];
+	reg [7 : 0] memory [0 : size - 1];
 	
-	assign	output_data = memory[input_address];
+	assign	output_data = {
+		memory[input_address],
+		memory[input_address + 1],
+		memory[input_address + 2],
+		memory[input_address + 3]
+	}
 
 endmodule
