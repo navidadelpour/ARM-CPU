33ddfe03169bcb78b61c23f8b3b11911e8faf3a9
navidAdelpour <navidadelpour@gmail.com>
2018-12-22 16:56:32 +0330

    adding read wire to data memory

--- DataMemory.v
+++ DataMemory.v
@@ -1,16 +1,19 @@
-module DataMemory(clock, write, input_address, input_data, output_data);
-	input clock, write;
+module DataMemory(clock, write, read, input_address, input_data, output_data);
+	input clock, write, read;
 	input [63:0] input_address, input_data;
 	output[63:0] output_data;
 
 	reg [63:0] memory [0:127];
 
-	assign output_data = memory[input_address];
 
 	always @ (posedge clock) begin
+	
+		if(read)
+			output_data = memory[input_address];
+
 		if(write)
 			memory[input_address] = input_data;
-			
+		
 	end
 
 endmodule
