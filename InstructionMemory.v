f87d63fcc2dd4404f847ecb52be1d0c824f81814
Saleh26 <bayatsaleh@gmail.com>
2018-12-25 22:29:59 +0330

    fixing initial instruction of instruction memory

--- InstructionMemory.v
+++ InstructionMemory.v
@@ -17,25 +17,11 @@ module InstructionMemory #(parameter size = 256) (input_address, output_data);
 		instruction3 = 32'h8b040086;
 		instruction4 = 32'hf80010a6;
 
-		memory[0 + 0] =	instruction1[1:0];
-		memory[0 + 1] = instruction1[3:2];
-		memory[0 + 2] = instruction1[5:4];
-		memory[0 + 3] = instruction1[7:6];
+		{memory[0 + 3], memory[0 + 2], memory[0 + 1], memory[0 + 0]} = 32'h8b1f03e5;
+		{memory[4 + 3], memory[4 + 2], memory[4 + 1], memory[4 + 0]} = 32'hf84000a4;
+		{memory[8 + 3], memory[8 + 2], memory[8 + 1], memory[8 + 0]} = 32'h8b040086;
+		{memory[12 + 3], memory[12 + 2], memory[12 + 1], memory[12 + 0]} = 32'hf80010a6;
 
-		memory[4 + 0] =	instruction2[1:0];
-		memory[4 + 1] = instruction2[3:2];
-		memory[4 + 2] = instruction2[5:4];
-		memory[4 + 3] = instruction2[7:6];
-
-		memory[8 + 0] =	instruction3[1:0];
-		memory[8 + 1] = instruction3[3:2];
-		memory[8 + 2] = instruction3[5:4];
-		memory[8 + 3] = instruction3[7:6];
-
-		memory[12 + 0] = instruction4[1:0];
-		memory[12 + 1] = instruction4[3:2];
-		memory[12 + 2] = instruction4[5:4];
-		memory[12 + 3] = instruction4[7:6];
 
 	end
 
