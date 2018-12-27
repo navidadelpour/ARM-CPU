fd3d8307b6955fec6a8df382393cde35dbaebd43
Saleh26 <bayatsaleh@gmail.com>
2018-12-27 20:18:17 +0330

    Warnings and bugs fixed

--- RegisterBank.v
+++ RegisterBank.v
@@ -6,7 +6,7 @@ module RegisterBank (
 
 	input clock;
 	input write;
-	input [5 : 0] input_address_1, input_address_2, input_address_3;
+	input [4 : 0] input_address_1, input_address_2, input_address_3;
 	input [63 : 0] input_data;
 	
 	output[63 : 0] output_data_1, output_data_2;
@@ -15,6 +15,14 @@ module RegisterBank (
 
 	assign output_data_1 = registers[input_address_1];
 	assign output_data_2 = registers[input_address_2];
+
+	integer i;
+
+	initial begin
+		for(i = 0; i < 32; i = i + 1)
+			registers[i] = 0;
+		registers[31] = 3;
+	end
 
 	always @ (posedge clock) begin
 		if(write)
--- clock.v
+++ clock.v
@@ -3,13 +3,13 @@ module Clock (clock);
 	output reg clock;
 
 	initial begin
-		clock = 0;
+		clock = 1;
 	end
 
 	always
 		begin 
-			#100 clock = 1;
 			#100 clock = 0;
+			#100 clock = 1;
   		end
 
 endmodule
