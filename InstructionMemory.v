01f7a016c2261a686c4abd2b3dc64bdbf6288972
Saleh26 <bayatsaleh@gmail.com>
2018-12-27 18:11:07 +0330

    cleaning instuction memory

--- InstructionMemory.v
+++ InstructionMemory.v
@@ -12,16 +12,10 @@ module InstructionMemory #(parameter size = 256) (input_address, output_data);
 
 	initial begin
 
-		instruction1 = 32'h8b1f03e5;
-		instruction2 = 32'hf84000a4;
-		instruction3 = 32'h8b040086;
-		instruction4 = 32'hf80010a6;
-
 		{memory[0 + 3], memory[0 + 2], memory[0 + 1], memory[0 + 0]} = 32'h8b1f03e5;
 		{memory[4 + 3], memory[4 + 2], memory[4 + 1], memory[4 + 0]} = 32'hf84000a4;
 		{memory[8 + 3], memory[8 + 2], memory[8 + 1], memory[8 + 0]} = 32'h8b040086;
 		{memory[12 + 3], memory[12 + 2], memory[12 + 1], memory[12 + 0]} = 32'hf80010a6;
-
 
 	end
 
